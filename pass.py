68c8657670535032f23675b878fc13e79b2d16f6
ouadia sabih <143357325+starGt324@users.noreply.github.com>
2025-04-08 16:59:15 +0000

    remove commant

--- pass.py
+++ pass.py
@@ -1,7 +1,7 @@
 import random
 user_range:int=int(input("how much string you want in your password :"))
 list_pw=["a","b","c","E","f","1","2","3","4","5","6","7","8","9","0","@","!","\#"]
-#print(list_chois)
+
 
 user_password=""
 for pointer in range(user_range):
