913a439c8f276d4105fae2481f52a94c1a6d2a1b
ouadia sabih <143357325+starGt324@users.noreply.github.com>
2025-04-23 15:06:56 +0000

    test new update 0.1.4v in new sorce code.

---
+++ password_gen.py
@@ -0,0 +1,45 @@
+#new arch
+# mod by ouadia sabih in 23/4/25
+import random
+import time
+import secrets
+from datetime import datetime
+import string
+from colorama import Fore,Back,Style
+
+random_id=random.randint(999,99999999999999999)
+try:
+    userinput=int(input("add a length of your password :"))
+
+except ValueError as eror:
+    print(eror)
+    print("the input must be a number next time.")
+    exit()
+
+else:
+    def gen_pw_string():
+        global gen
+        gen=secrets.token_urlsafe(userinput)
+        return gen
+    
+    print(Fore.LIGHTGREEN_EX,gen_pw_string())
+
+    def gen_pw_num():
+        global gen_int
+        print("by defult gen 12digi for security.")
+        gen_int=random.randint(userinput,999999999999)
+        return gen_int
+
+    print(Fore.LIGHTBLUE_EX,gen_pw_num())
+    Style.RESET_ALL
+
+
+##saving################################################################################
+    with open("password.txt","a+") as file:
+        file.write(f"---------------------------------------------------\n")
+        file.write(f"ID:{random_id}\n")
+        file.write(f"Date cration:{datetime.now()}\n")
+        file.write(f"password: {gen_pw_string()}\n")
+        time.sleep(0.3)
+    print(Fore.YELLOW,"operation complite data save in password.txt")
+    Style.RESET_ALL
